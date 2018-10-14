7d33dfc64f0ae2a9d08a00e985d0363e8c3d7109
Finley McIlwaine <finleymcilwaine@gmail.com>
2018-10-14 14:13:09 -0600

    Add files via upload

---
+++ VectorDriver.cpp
@@ -0,0 +1,114 @@
+/* File: VectorDriver.cpp
+ * Athr: Finley McIlwaine
+ * Date: October 14, 2018
+ *
+ * Desc: Exemplifies the use of the STL with vectors,
+ * iterators, and algorithms.
+ */
+
+#include "pch.h"
+#include "VectorDriver.h"
+#include <algorithm>
+
+int main() {
+	cout << "\n   ..:: B E G I N  S A M P L E  C O D E  ::.." << endl << endl;
+	vector<short> sample_vector(5);
+
+	cout << "new vector: ";
+	print(sample_vector);
+	cout << endl;
+
+	fill_vector(sample_vector);
+
+	cout << "filled vector: ";
+	print(sample_vector);
+	cout << endl;
+
+	cout << "sum of vector's elements: " << compute_sum(sample_vector) << endl;
+	cout << endl << "    ..::  E N D   S A M P L E  C O D E  ::.." << endl << endl;
+
+	// Testing my add_numbers function.
+	// Create and display empty vector.
+	vector<short>testVector(0);
+	cout << "Testing add_numbers() function:" << endl;
+	cout << "Empty vector:                    ";
+	print(testVector);
+	cout << endl;
+
+	// Call add_numbers and display result
+	add_numbers(testVector);
+	cout << "After first add_numbers() call:  ";
+	print(testVector);
+	cout << endl;
+
+	// Call add_numbers again and display result
+	add_numbers(testVector);
+	cout << "After second add_numbers() call: ";
+	print(testVector);
+	cout << endl;
+
+	// Testing my print_even function
+	// Clear testVector and print to test empty print.
+	testVector.clear();
+	cout << "\nTesting print_even() function:" << endl;
+	cout << "Empty vector:                ";
+	print_even(testVector);
+	cout << endl;
+
+	// Test print_even with vector = <0, 1, 2, 3, 4, 5>
+	testVector = { 0, 1, 2, 3, 4, 5 };
+	cout << "Vector = {0, 1, 2, 3, 4, 5}: ";
+	print_even(testVector);
+	cout << endl;
+
+	// Testing my is_present function
+	// Test with empty vector, value = 2
+	testVector.clear();
+	cout << "\nTesting is_present() function:" << endl;
+	cout << "Empty vector, value = 2:                ";
+	// Here we implement a simple print expression for is_present results.
+	cout << ((is_present(testVector, 2) == false) ? "FALSE" : "TRUE");
+	cout << endl;
+
+	// Test is_present with vector = <0, 1, 2, 3, 4, 5>, value = 2
+	testVector = { 0, 1, 2, 3, 4, 5 };
+	cout << "Vector = {0, 1, 2, 3, 4, 5}, value = 2: ";
+	cout << ((is_present(testVector, 2) == false) ? "FALSE" : "TRUE");
+	cout << endl;
+
+	// Test is_present with vector = <0, 1, 2, 3, 4, 5>, value = 6
+	cout << "Vector = {0, 1, 2, 3, 4, 5}, value = 6: ";
+	cout << ((is_present(testVector, 6) == false) ? "FALSE" : "TRUE");
+	cout << endl;
+
+	// Test sorting with iterators.
+	// Make a vector of five random numbers
+	testVector = { -6, 90, 29, -38, 0 };
+	cout << "\nTesting sort() with iterators:" << endl;
+	cout << "Unsorted: ";
+	print(testVector);
+	cout << endl;
+
+	// Initialize begin() and end() iterators.
+	std::vector<short>::iterator iterBegin = testVector.begin();
+	std::vector<short>::iterator iterEnd = testVector.end();
+
+	// Save unsorted vector
+	vector<short>unsorted = testVector;
+
+	// Call sort
+	sort(iterBegin, iterEnd);
+
+	// Output result
+	cout << "Sorted:   ";
+	print(testVector);
+	cout << endl;
+
+	// Simple test if sums are equal in sorted and unsorted
+	bool sumEqual = (compute_sum(unsorted) == compute_sum(testVector));
+	cout << "Sum of elements of unsorted and sorted vectors equal?: " << (sumEqual ? "YES" : "NO") << endl;
+	cout << endl;
+
+  system("pause");
+  return 0;
+};
---
+++ VectorDriver.h
@@ -0,0 +1,101 @@
+/* File: VectorDriver.h
+ * Athr: Finley McIlwaine
+ * Date: October 14, 2018
+ *
+ * Desc: provides the basic framework and examples for an introduction to
+ *       the STL::vector, iterators, and the algorithms facilities.
+ */
+typedef unsigned long ulong;
+
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+// fill vector that has pre-allocated space with consecutive numbers
+void fill_vector(vector<short> &data) {
+  for (ulong i=0; i<data.size(); i++) {
+    data.at(i) = i;
+  }
+}
+
+// print our vector in a fancy way
+void print(const vector<short> &data) {
+  if (data.empty()) {
+    cout << "<empty>";
+  } else {
+    cout << "<" << data.at(0);
+    for (ulong i=1; i<data.size(); i++) {
+      cout << ", " << data.at(i);
+    }
+    cout << ">";
+  }
+}
+
+// use iterator to sum the elements of a vector
+short compute_sum(const vector<short> &data) {
+  std::vector<short>::const_iterator iter = data.begin();
+
+  short sum = 0;
+  while (iter != data.end()) {
+    sum += *iter;
+    iter++;
+  }
+
+  return sum;
+}
+
+// This function adds ten random numbers of type short
+// to the end of the input vector.
+void add_numbers(vector<short> &data) {
+	// Declare our random short variable.
+	short randomShort;
+
+	// Loop to add random numbers to the vector.
+	for (ulong i = 0; i < 10; i++)
+	{
+		// Generate a random number.
+		randomShort = rand() % 100 + 1;
+
+		// Copy the random number to the end of the vector.
+		data.push_back(randomShort);
+	}
+}
+
+// This prints the elements at even indices in a vector.
+// If the vector is empty, it prints <empty>.
+void print_even(const vector<short> &data) {
+	// Test if empty.
+	if (data.empty()) {
+		// Is empty. Print <empty>.
+		cout << "<empty>";
+	}
+	else {
+		// Is not empty. Print elements at even indices.
+		cout << "<" << data.at(0);
+		for (ulong i = 2; i < data.size(); i += 2)
+		{
+			cout << ", " << data.at(i);
+		}
+		cout << ">";
+	}
+}
+
+// This function returns true if value is a member of data, false otherwise.
+bool is_present(const vector<short> &data, short value) {
+	// Initialize our iterator
+	std::vector<short>::const_iterator iter = data.begin();
+
+	// Test each element of the vector.
+	while (iter != data.end()) {
+		// Returns true if dereferenced iterator == value,
+		// otherwise increment iter.
+		if (*iter == value) { return true; }
+		iter++;
+	}
+
+	// No match found, return false.
+	return false;
+}
